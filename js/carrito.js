ac67a25173a3285034e10efb1c139e402498127b
Rene Leyva <sinmatonesaporfavor@gmail.com>
2017-02-07 13:23:40 -0600

    Vista Carrito

---
+++ js/carrito.js
@@ -0,0 +1,24 @@
+//Toma los precios de la tabla y calcula el total 
+function calculateTotal() {
+	var total = 0;
+	$("tr.item").each( function(index) {
+		var price = $(this).find(".price").data("price");
+		total += price + 100; //+100 del envio
+	});
+	$('#total').html("<b>" + "Total: " + total + "</b>");
+	if (total > 0) {
+		$(".continue button").removeClass("disabled");
+	} else {
+		$(".continue button").addClass("disabled");
+	}
+}
+
+jQuery(document).ready(function($) {
+	calculateTotal();
+
+	$('.eliminar').click(function(e) {
+		$(this).closest('tr').hide('slow').remove();
+		calculateTotal();
+	});
+
+});
