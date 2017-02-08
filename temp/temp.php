fc0c7330d44f341ab447718dee6471ed3da08ee9
Luis Pulido Alvarez <Luis Pulido Alvarez>
2017-02-08 16:47:25 -0600

    Es que la amo bebé

---
+++ temp/temp.php
@@ -0,0 +1,3 @@
+<img src="portada/images.jpg">
+<img src="portada/images.jpg">
+<img src="<?php echo 'portada/'.$book?>">
---
+++ temp/welcome.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Bienvenida</title>
+	</head>
+	<body>
+		<?php echo $Bienvenida; ?>
+		<p><a href="?showbook"> Mostrar todos los libros </a></p>
+		<p><a href="?addbook"> Agregar un libro </a></p>
+	</body>
+</html>
