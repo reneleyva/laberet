d35cfa2466cc9eb23840d02a4e20d414cafef41a
edmorua <eduardo.r.morua@gmail.com>
2017-03-02 15:58:58 -0600

    Andrea de mi corazon

--- temp/Libro.php
+++ temp/Libro.php
@@ -66,9 +66,9 @@ class Libro {
 		}
 
 	}
-	public function compareTo($book1):
+	public function compareTo($book1)
 	{
-		return this->id == book1.getId();
+		return $this->id == book1.getId();
 	}
 	public function getLibrosRelacionados($id) {
 		include "../../conexion.php";
