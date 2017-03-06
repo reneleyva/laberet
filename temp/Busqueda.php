e6fc53bafc3b51242d7ae40a0e89fdd899b4b83c
Luis Pulido Alvarez <Luis Pulido Alvarez>
2017-03-06 16:34:36 -0600

    Pene de toro

--- temp/Busqueda.php
+++ temp/Busqueda.php
@@ -4,31 +4,56 @@
 * Clase para Libro. 
 */
 class Busqueda {
-	/*
-	private $id;
-	private $autor;
-	private $titulo;
-	private $isbn;
-	private $fechaAdicion;
-	private $precio;
-	private $fotoFrente;
-	private $fotoAtras;
-	private $tags;
-	private $idLibreria;
-	*/
-
-	//Vacio 
 	function __construct() {}
 
-	function Busca(){
+	function buscaGeneral($key){
 		include "../../conexion.php";
 		$sql = "SELECT * FROM Libro;";
 		$result = $pdo->query($sql);
 		$row = $result->fetch();
-		$books[] = null;
+		$books[] = array();
 		while ($row = $result->fetch()){
 			//$vacio = False;
 			$book = new Libro();
 			$book->fill($row);
+			array_push($books,$book);
 		}
 	}
+
+	public function getLibrosRelacionados($id){
+		include "../../conexion.php";
+		 // Seleccionamos el libro que nos pasan como ref.
+		$book = getLibro($id);
+		// Donde se guardarán los libros relacionados
+		$books[] = array(); 
+		// Se busca por autor
+		$sql = "SELECT * FROM Libro WHERE autor = '".$book->getAutor()."' AND 
+		        titulo != '".$book->getTitulo()."';";
+		$result = $pdo->query($sql);
+		$titulos[] = Null;
+		while ($libro = $result->fetch()){
+			$bookAux = new Libro();
+			$bookAux->fill($libro);
+			array_push($books,$bookAux);
+		}
+		// Separa los tags
+		if(!$books){
+			$tags = Null;
+			$titulos[] = Null;
+		}
+		$tags = explode(" ", trim($book->getTags(), " "));
+		foreach ($tags as $tag) {
+			if ($tag != "") {
+				$sql = "SELECT * FROM Libro WHERE lower(tags) LIKE 
+			        lower('%".$tag."%') AND titulo != '".$book->getTitulo()."';";
+			    $result = $pdo->query($sql);
+			    while ($libro = $result->fetch()){
+			    	// Verifica que no se repita el título
+			    	if (!(in_array($book->getTitulo(), $titulos))){
+			    		array_push($books,$libro);
+			    	}
+			    }
+			}
+		}
+	}
+}
--- temp/Libro.php
+++ temp/Libro.php
@@ -66,43 +66,7 @@ class Libro {
 		}
 
 	}
-	public function getLibrosRelacionados($id){
-		include "../../conexion.php";
-		 // Seleccionamos el libro que nos pasan como ref.
-		$book = getLibro($id);
-		// Donde se guardarán los libros relacionados
-		$books[] = Null; 
-		// Se busca por autor
-		$sql = "SELECT * FROM Libro WHERE autor = '".$book->getAutor()."' AND 
-		        titulo != '".$book->getTitulo()."';";
-		$result = $pdo->query($sql);
-		$titulos[] = Null;
-		while ($libro = $result->fetch()){
-			$bookAux = new Libro();
-			$bookAux->fill($libro);
-			//Aquí no sé como agregarlo :v
-		}
-		// Separa los tags
-		if(!$books){
-			$tags = Null;
-			$titulos[] = Null;
-		}
-		$tags = explode(" ", trim($book->getTags(), " "));
-		foreach ($tags as $tag) {
-			if ($tag != "") {
-				$sql = "SELECT * FROM Libro WHERE lower(tags) LIKE 
-			        lower('%".$tag."%') AND titulo != '".$book->getTitulo()."';";
-			    $result = $pdo->query($sql);
-			    while ($libro = $result->fetch()){
-			    	// Verifica que no se repita el título
-			    	if (!(in_array($book->getTitulo(), $titulos))){
-			    		//Agrega libro again
-			    	}
-			    }
-			}
-		}
-	}
-
+	
 	//GETTERS Y SETTERS. 
 	public function getId(){
 		return $this->id;
