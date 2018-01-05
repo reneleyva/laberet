bc7bd544214d5a931b6c93ea17ea84497cc22b43
Rene GL <sinmatonesaporfavor@gmail.com>
2018-01-05 00:45:19 -0600

    Procfile

--- conexion.php
+++ conexion.php
@@ -15,19 +15,19 @@ if (!$con) {
     echo "Error: " . mysqli_connect_error();
 	exit();
 }
-$api_key = "176317843429194";
-$cloud = "dzu2umeba"; 
-$api_secret = "SqdUW7QjZaFri1WJo93DUiP1eyo";
+// $api_key = "176317843429194";
+// $cloud = "dzu2umeba"; 
+// $api_secret = "SqdUW7QjZaFri1WJo93DUiP1eyo";
 
-putenv("CLOUD_NAME=$cloud");
-putenv("API_KEY=$api_key");
-putenv("API_SECRET=$api_secret");
- LOCAL HOST CONEXION! 
-$con = @mysqli_connect('localhost', 'root', 'root', 'laberet');
-// Para los acentos.
-$sql = " SET NAMES UTF8;";
-mysqli_query($con, $sql);
-if (!$con) {
-    echo "Error: " . mysqli_connect_error();
-	exit();
-}
+// putenv("CLOUD_NAME=$cloud");
+// putenv("API_KEY=$api_key");
+// putenv("API_SECRET=$api_secret");
+//  LOCAL HOST CONEXION! 
+// $con = @mysqli_connect('localhost', 'root', 'root', 'laberet');
+// // Para los acentos.
+// $sql = " SET NAMES UTF8;";
+// mysqli_query($con, $sql);
+// if (!$con) {
+//     echo "Error: " . mysqli_connect_error();
+// 	exit();
+// }
